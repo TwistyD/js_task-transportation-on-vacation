0f6464a341420fe091142dc706240d5c1fc163a7
Dima Poklad <twistdeni@gmail.com>
2026-04-01 13:04:26 +0300

    Added calculatRentalCost task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,26 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  // write code here
+function calculateRentalCost(numberOfDays) {
+  const DAILY_RATE = 40;
+  const LONG_TERM_DAYS = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const MID_TERM_DAYS = 3;
+  const MID_TERM_DISCOUNT = 20;
+
+  const basePrice = numberOfDays * DAILY_RATE;
+
+  if (numberOfDays >= LONG_TERM_DAYS) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (numberOfDays >= MID_TERM_DAYS) {
+    return basePrice - MID_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
+
+
 
 module.exports = calculateRentalCost;
